81892c46ac78b173f75e3ad902700db9a4048191
kis <2330029579@qq.com>
2024-03-26 18:40:10 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['antd', '@ant-design/icons'],
+	transpilePackages: ['antd', '@ant-design/icons', 'mathjax-full']
 };
 
 export default nextConfig;
