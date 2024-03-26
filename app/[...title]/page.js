7920301cd3c285b9e85bb24734f716b2a5e39ba4
kis <2330029579@qq.com>
2024-03-26 14:19:52 +0800

    Update page.js

--- app/[...title]/page.js
+++ app/[...title]/page.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+// import fs from 'fs';
 import path from 'path';
 import { markdownToHtml } from '../../utils/markdownIt';
 // import parseDocFile from '../../utils/parseDocFile';
