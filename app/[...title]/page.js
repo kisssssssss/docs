8ce186073824d5f2a1caf31354601024e222c209
kis <2330029579@qq.com>
2024-03-26 13:38:43 +0800

    update

--- app/[...title]/page.js
+++ app/[...title]/page.js
@@ -1,10 +1,11 @@
-import parseDocFile from '../../utils/parseDocFile';
+// import parseDocFile from '../../utils/parseDocFile';
 
-import Article from '../../components/Article';
+// import Article from '../../components/Article';
 
 export default async function Page(props) {
-	const title = props.params.title.map(item => decodeURI(item));
-	const data = await parseDocFile(title);
+	// const title = props.params.title.map(item => decodeURI(item));
+	// const data = await parseDocFile(title);
 
-	return <Article content={data.content} />;
+	// return <Article content={data.content} />;
+	return <h1>123</h1>
 }
--- utils/parseDocFile.js
+++ utils/parseDocFile.js
@@ -6,7 +6,8 @@ import { markdownToHtml } from '../utils/markdownIt';
 export default async function parseDocFile(mdPath) {
 	// 获取文档所在路径
 	let finalMdPath = path.join(process.cwd(), 'docs', `${path.join(...mdPath)}.md`);
-
+	fetch(finalMdPath).then(() => {res.text().then(res => console.log(res))});
+	console.log(finalMdPath);
 	try {
 		let source = await fs.promises.readFile(finalMdPath, 'utf-8');
 		return markdownToHtml(source);
