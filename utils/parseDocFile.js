14baf1c138d4d66ef540677ae6922991e9fa2444
kis <2330029579@qq.com>
2024-03-26 13:26:57 +0800

    Update parseDocFile.js

--- utils/parseDocFile.js
+++ utils/parseDocFile.js
@@ -5,17 +5,13 @@ import { markdownToHtml } from '../utils/markdownIt';
 
 export default async function parseDocFile(mdPath) {
 	// 获取文档所在路径
-	let finalMdPath = `${path.join(...mdPath)}.md`;
-	if (finalMdPath.startsWith('/')) {
-		finalMdPath = finalMdPath.slice(1);
-	}
-	finalMdPath = path.join(process.cwd(), 'docs', finalMdPath);
+	let finalMdPath = path.join(process.cwd(), 'docs', `${path.join(...mdPath)}.md`);
 
 	try {
 		let source = await fs.promises.readFile(finalMdPath, 'utf-8');
 		return markdownToHtml(source);
 	} catch (e) {
-		console.error(e);
+		console.error('读取文件错误', finalMdPath, e);
 		return null;
 	}
 }
