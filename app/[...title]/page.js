ef90b17572335d0fbab71f6817e1bca51257af17
kis <2330029579@qq.com>
2024-03-26 13:53:03 +0800

    update

--- app/[...title]/page.js
+++ app/[...title]/page.js
@@ -1,15 +1,23 @@
+import fs from 'fs';
 import path from 'path';
+import { markdownToHtml } from '../../utils/markdownIt';
 // import parseDocFile from '../../utils/parseDocFile';
 
 // import Article from '../../components/Article';
 
 export default async function Page(props) {
 	const title = props.params.title.map(item => decodeURI(item));
+	const mdPath = path.join(process.cwd(), 'docs', `${path.join(...title)}.md`);
+
+	let md;
+	try {
+		const source = await fs.promises.readFile(mdPath, 'utf-8');
+		md = markdownToHtml(source);
+	} catch (err) {}
 
 	// const data = await parseDocFile(title);
 
 	// return <Article content={data.content} />;
 
-	let finalMdPath = path.join(process.cwd(), 'docs', `${path.join(...title)}.md`);
-	return <h1>{finalMdPath}</h1>
+	return <h1>{md.content}</h1>;
 }
