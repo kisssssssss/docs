9d087cc7d5bfed20c6f144ee017e5f76da6832fa
kis <2330029579@qq.com>
2024-03-26 17:23:31 +0800

    Update page.js

--- app/[...title]/page.js
+++ app/[...title]/page.js
@@ -1,22 +1,25 @@
 import path from 'path';
 import fs from 'fs';
-import { markdownToHtml } from '../utils/markdownIt';
+// import { markdownToHtml } from '../utils/markdownIt';
 // import parseDocFile from '../../utils/parseDocFile';
 
 // import Article from '../../components/Article';
+
+import MarkdownIt from 'markdown-it';
+const md = new MarkdownIt()
 
 export default async function Page(props) {
 	const title = props.params.title.map(item => decodeURI(item));
 	const mdPath = path.join(process.cwd(), 'docs', `${path.join(...title)}.md`);
 
-	let md;
+	let source;
 	try {
-		md = await fs.promises.readFile(mdPath, 'utf-8');
+		source = await fs.promises.readFile(mdPath, 'utf-8');
 	} catch (_) {}
 
 	// const data = await parseDocFile(title);
 
 	// return <Article content={data.content} />;
 
-	return <h1>{md}</h1>;
+	return <h1>{md.render(source)}</h1>;
 }
