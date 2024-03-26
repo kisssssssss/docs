b4f9b460427c51c6a4005bfdc786a755a0cfc4f5
kis <2330029579@qq.com>
2024-03-26 13:46:18 +0800

    Update page.js

--- app/[...title]/page.js
+++ app/[...title]/page.js
@@ -1,11 +1,15 @@
+import path from 'path';
 // import parseDocFile from '../../utils/parseDocFile';
 
 // import Article from '../../components/Article';
 
 export default async function Page(props) {
 	const title = props.params.title.map(item => decodeURI(item));
+
 	// const data = await parseDocFile(title);
 
 	// return <Article content={data.content} />;
-	return <h1>{title}</h1>
+
+	let finalMdPath = path.join(process.cwd(), 'docs', `${path.join(...title)}.md`);
+	return <h1>{finalMdPath}</h1>
 }
