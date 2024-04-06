0d683e2b0e4ac15f9f4f96ea874b732f9980d600
kis <2330029579@qq.com>
2024-04-06 11:31:30 +0800

    update

--- components/Article.js
+++ components/Article.js
@@ -2,12 +2,12 @@
 import { useRouter } from 'next/navigation';
 import { FloatButton } from 'antd';
 import { HomeOutlined } from '@ant-design/icons';
-export default function Article({content}) {
+export default function Article({ content }) {
 	const router = useRouter();
 	return (
 		<>
 			<article
-				className='prose prose-pre:font-[CaskaydiaCoveNerdFontMono] mx-auto px-6 my-20 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-[896px] xl:max-w-screen-[1120px]'
+				className={`prose prose-pre:font-[CaskaydiaCoveNerdFontMono] prose-code:before:content-[''] prose-code:after:content-[''] prose-code:font-[CaskaydiaCoveNerdFontMono] prose-a:underline-offset-[6px] mx-auto px-6 my-20 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-[896px] xl:max-w-screen-[1120px]`}
 				dangerouslySetInnerHTML={{ __html: content }}></article>
 			<FloatButton
 				type='primary'
