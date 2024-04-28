5d5511ce22dffdaed6cd2085c8037deb90cb6c5e
kis <2330029579@qq.com>
2024-04-28 21:06:22 +0800

    update

--- components/setting/live2dSetting.js
+++ components/setting/live2dSetting.js
@@ -1,9 +1,11 @@
 'use client';
-import React, { memo, useCallback } from 'react';
+import React, { memo, useCallback, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Cookies from 'js-cookie';
-import { Switch } from 'antd';
+import { Switch, Card, Divider, Space } from 'antd';
 import { SwapLeftOutlined } from '@ant-design/icons';
+
+const { Meta } = Card;
 
 const live2d = memo(() => {
 	const router = useRouter();
@@ -14,10 +16,39 @@ const live2d = memo(() => {
 		setTimeout(() => window.location.reload(), 100);
 	}, []);
 
+	const ModelList = useCallback(() => {
+		let list = localStorage.getItem('modelsList');
+		if (list) {
+			list = JSON.parse(list);
+			return (
+				<div>
+					<h2>当前 Live2D 数量：{list.length}</h2>
+					<Space wrap size='large'>
+						{list.map(item => (
+							<Card
+								key={item.name}
+								hoverable
+								style={{ width: 160 }}
+								styles={{
+									cover: { width: '160px', height: '90px', overflow: 'hidden' },
+									body: { padding: '12px 16px' }
+								}}
+								cover={<img className='my-0 object-scale-down  w-[160px] h-[90px]' alt='' src={item.cover} loading='lazy' />}>
+								<Meta title={item.title} />
+							</Card>
+						))}
+					</Space>
+				</div>
+			);
+		} else {
+			return <></>;
+		}
+	}, []);
+
 	return (
 		<div className='prose mx-auto px-6 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-[896px] xl:max-w-screen-[1120px] py-20'>
 			<SwapLeftOutlined
-				className='-ml-8 hover:text-violet-500 cursor-pointer'
+				className='-ml-8 hover:text-violet-500 cursor-pointer text-[25px]'
 				onClick={() => {
 					router.push('/');
 				}}
@@ -26,6 +57,8 @@ const live2d = memo(() => {
 				<p className='inline-block text-xl font-bold'>显示 Live2D</p>
 				<Switch defaultChecked={Cookies.get('live2d_open') == 'true'} onChange={change} />
 			</div>
+			<Divider />
+			<ModelList />
 		</div>
 	);
 });
