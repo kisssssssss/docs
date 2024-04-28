93a27dd2694689190944a23c88f25a96c7568dc1
kis <2330029579@qq.com>
2024-04-29 00:26:45 +0800

    update

--- components/Live2d.js
+++ components/Live2d.js
@@ -9,55 +9,59 @@ const Live2d = memo(() => {
 	useEffect(() => {
 		(async function () {
 			if (Cookies.get('live2d_open') == 'true') {
-				// 获取模型列表
-				const modelsList = await (await fetch('/ModelList.json')).json();
+				try {
+					// 获取并存储 模型信息
+					const response = await (await fetch('/ModelList.json')).json();
+					localStorage.setItem('modelsList', JSON.stringify(response));
 
-				// 存储模型列表以便在设置页面中展示
-				localStorage.setItem('modelsList', JSON.stringify(modelsList));
-
-				// 加载模型
-				const oml2d = loadOml2d({
-					primaryColor: '#8b5cf6',
-					models: modelsList.map(item => {
-						return {
+					// 生成模型数组
+					let models = [];
+					for (const key in response) {
+						const temp = response[key].map(item => ({
 							name: item.name,
 							position: item.position.map(e => Number(e)),
 							scale: Number(item.scale),
 							stageStyle: item.stageStyle,
-							path: `/model/${item.name}/${item.name}.model3.json`
-						};
-					})
+							path: `/model/${key}/${item.name}/${item.name}.model3.json`
+						}));
+						models.push(...temp);
+					}
 
-					// 模型筛选
-					// models: [
-					// 	...publicModel.map(item => {
-					// 		return {
-					// 			path: `/model/${item}/${item}.model3.json`,
-					// 			name: item,
-					// 			position: [-100, -150],
-					// 			scale: 0.07,
-					// 			stageStyle: { height: 450, width: 300 }
-					// 		};
-					// 	})
-					// ]
-				});
+					// 加载模型
+					const oml2d = loadOml2d({
+						primaryColor: '#8b5cf6',
+						models
+						// 模型筛选
+						// models: [
+						// 	...publicModel.map(item => {
+						// 		return {
+						// 			path: `/model/${item}/${item}.model3.json`,
+						// 			name: item,
+						// 			position: [-100, -150],
+						// 			scale: 0.07,
+						// 			stageStyle: { height: 450, width: 300 }
+						// 		};
+						// 	})
+						// ]
+					});
 
-				// oml2d.onLoad(status => {
-				// 	switch (status) {
-				// 		case 'success':
-				// 			console.log('模型: ' + oml2d.model.name + '加载成功');
-				// 			console.log(oml2d.modelIndex);
-				// 			oml2d.showModelHitAreaFrames();
-				// 			return;
-				// 		case 'fail':
-				// 			console.log('模型: ' + oml2d.model.name + '加载失败');
-				// 			oml2d.loadNextModel();
-				// 			return;
-				// 		case 'loading':
-				// 			console.log('模型: ' + oml2d.model.name + '正在加载中');
-				// 			return;
-				// 	}
-				// });
+					// oml2d.onLoad(status => {
+					// 	switch (status) {
+					// 		case 'success':
+					// 			console.log('模型: ' + oml2d.model.name + '加载成功');
+					// 			console.log(oml2d.modelIndex);
+					// 			oml2d.showModelHitAreaFrames();
+					// 			return;
+					// 		case 'fail':
+					// 			console.log('模型: ' + oml2d.model.name + '加载失败');
+					// 			oml2d.loadNextModel();
+					// 			return;
+					// 		case 'loading':
+					// 			console.log('模型: ' + oml2d.model.name + '正在加载中');
+					// 			return;
+					// 	}
+					// });
+				} catch (error) {}
 			}
 		})();
 	}, []);
--- components/setting/live2dSetting.js
+++ components/setting/live2dSetting.js
@@ -1,48 +1,80 @@
 'use client';
-import React, { memo, useCallback, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import React, { memo, useCallback } from 'react';
+import { Switch, Card, Divider, Space, FloatButton } from 'antd';
+import { SwapLeftOutlined, ArrowUpOutlined } from '@ant-design/icons';
 import Cookies from 'js-cookie';
-import { Switch, Card, Divider, Space } from 'antd';
-import { SwapLeftOutlined } from '@ant-design/icons';
 
 const { Meta } = Card;
 
 const live2d = memo(() => {
 	const router = useRouter();
 
+	// 启用/禁用 Live2d
 	const change = useCallback(checked => {
 		Cookies.set('live2d_open', checked, { expires: 365 });
 		// 通过 setTimeout 防止 Switch 卡顿
 		setTimeout(() => window.location.reload(), 100);
 	}, []);
 
+	// 模型列表
 	const ModelList = useCallback(() => {
-		let list = localStorage.getItem('modelsList');
-		if (list) {
-			list = JSON.parse(list);
-			return (
-				<div>
-					<h2>当前 Live2D 数量：{list.length}</h2>
+		if (Cookies.get('live2d_open') != 'true') return <></>;
+
+		const list = JSON.parse(localStorage.getItem('modelsList') || '{}');
+
+		const renderResult = [];
+		const keyMap = { Azur: '碧蓝航线' };
+
+		// 获取模型预览图
+		let count = 0;
+		for (const key in list) {
+			count += list[key].length;
+
+			renderResult.push(
+				<div key={key} id={key}>
+					<p className='text-lg font-bold'>{keyMap[key]}</p>
 					<Space wrap size='large'>
-						{list.map(item => (
+						{list[key].map(item => (
 							<Card
 								key={item.name}
 								hoverable
 								style={{ width: 160 }}
 								styles={{
-									cover: { width: '160px', height: '90px', overflow: 'hidden' },
+									cover: { width: '160px', height: '160px', overflow: 'hidden' },
 									body: { padding: '12px 16px' }
 								}}
-								cover={<img className='my-0 object-scale-down  w-[160px] h-[90px]' alt='' src={item.cover} loading='lazy' />}>
-								<Meta title={item.title} />
+								cover={<img className='my-0 object-cover  w-[160px] h-[160px]' alt='' src={item.cover} loading='lazy' />}>
+								<Meta title={item.title} description={item.description} />
 							</Card>
 						))}
 					</Space>
 				</div>
 			);
-		} else {
-			return <></>;
 		}
+
+		// 获取模型数量及类型
+		renderResult.unshift(
+			<div key={'count'}>
+				<p className='text-xl font-bold'>当前 Live2D 数量：{count}</p>
+				<p className='text-base text-[#00000073] font-semibold'>
+					类型：
+					{Object.keys(list).map(key => (
+						<span
+							key={`type_${key}`}
+							className='hover:text-violet-500 hover:underline underline-offset-4 cursor-pointer'
+							onClick={() => {
+								document.getElementById(key).scrollIntoView();
+							}}>
+							{keyMap[key]}
+						</span>
+					))}
+				</p>
+				<Divider />
+			</div>
+		);
+
+		return renderResult;
 	}, []);
 
 	return (
@@ -59,6 +91,7 @@ const live2d = memo(() => {
 			</div>
 			<Divider />
 			<ModelList />
+			<FloatButton type='primary' icon={<ArrowUpOutlined />} onClick={() => scrollTo(0, 0)}></FloatButton>
 		</div>
 	);
 });
