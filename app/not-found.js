7670e0806b9b22b06cf06809f4cdb8c9ac40ba5b
kis <2330029579@qq.com>
2024-05-01 23:10:11 +0800

    update

--- app/not-found.js
+++ app/not-found.js
@@ -3,9 +3,9 @@ import Link from 'next/link';
 export default function NotFound() {
 	return (
 		<div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
-			<div className='flex items-center'>
+			<div className='flex items-center dark:text-gray-200/90'>
 				<h1 className='inline-block'>404</h1>
-				<div className='border-solid pl-5 ml-4 border-l-gray-950 border-l-2'>
+				<div className='border-solid pl-5 ml-4 border-l-gray-950 dark:border-l-gray-50 border-l-2'>
 					<h2 className='inline-block text-2xl'>Not Found</h2>
 					<p className='my-1'>Could not find requested page</p>
 					<Link href='/'>👉 Return Home</Link>
